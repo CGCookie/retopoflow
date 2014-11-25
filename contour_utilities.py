5b2bcc1c0cf6066812672cf52febcf35df43e204
Jonathan Williamson <jonathan@cgcookie.com>
2014-11-25 14:14:18 -0600

    Remove duplicated draw functions. Moved to lib/common_drawing. Improves #1

--- contour_utilities.py
+++ contour_utilities.py
@@ -63,23 +63,6 @@ def bgl_col(rgb, alpha):
     
     return color
 
-def draw_points(context, points, color, size):
-    '''
-    draw a bunch of dots
-    args:
-        points: a list of tuples representing x,y SCREEN coordinate eg [(10,30),(11,31),...]
-        color: tuple (r,g,b,a)
-        size: integer? maybe a float
-    '''
-
-    bgl.glColor4f(*color)
-    bgl.glPointSize(size)
-    bgl.glBegin(bgl.GL_POINTS)
-    for coord in points:  
-        bgl.glVertex2f(*coord)  
-    
-    bgl.glEnd()   
-    return
 
 def edge_loops_from_bmedges(bmesh, bm_edges):
     """
@@ -355,25 +338,6 @@ def pi_slice(x,y,r1,r2,thta1,thta2,res,t_fan = False):
             
     return(points)
 
-def draw_outline_or_region(mode, points, color):
-        '''  
-        arg: 
-        mode - either bgl.GL_POLYGON or bgl.GL_LINE_LOOP
-        color - will need to be set beforehand using theme colors. eg
-        bgl.glColor4f(self.ri, self.gi, self.bi, self.ai)
-        '''
-        
-        bgl.glColor4f(color[0],color[1],color[2],color[3])
-        if mode == 'GL_LINE_LOOP':
-            bgl.glBegin(bgl.GL_LINE_LOOP)
-        else:
-            bgl.glBegin(bgl.GL_POLYGON)
- 
-        # start with corner right-bottom
-        for i in range(0,len(points)):
-            bgl.glVertex2f(points[i][0],points[i][1])
- 
-        bgl.glEnd()
 
 def arrow_primitive(x,y,ang,tail_l, head_l, head_w, tail_w):
     
@@ -459,113 +423,7 @@ def simple_circle(x,y,r,res):
         points[i]=Vector((r * x1 + x, r * y1 + y))
            
     return(points)     
-    
-def draw_3d_points(context, points, color, size):
-    '''
-    draw a bunch of dots
-    args:
-        points: a list of tuples representing x,y SCREEN coordinate eg [(10,30),(11,31),...]
-        color: tuple (r,g,b,a)
-        size: integer? maybe a float
-    '''
-    points_2d = [location_3d_to_region_2d(context.region, context.space_data.region_3d, loc) for loc in points]
 
-    bgl.glColor4f(*color)
-    bgl.glPointSize(size)
-    bgl.glBegin(bgl.GL_POINTS)
-    for coord in points_2d:
-        #TODO:  Debug this problem....perhaps loc_3d is returning points off of the screen.
-        if coord:
-            bgl.glVertex2f(*coord)  
-        else:
-            print('how the f did nones get in here')
-            print(coord)
-    
-    bgl.glEnd()   
-    return
-
-# Depreciated
-def draw_polyline_from_points(context, points, color, thickness, LINE_TYPE):
-    '''
-    a simple way to draw a line
-    args:
-        points: a list of tuples representing x,y SCREEN coordinate eg [(10,30),(11,31),...]
-        color: tuple (r,g,b,a)
-        thickness: integer? maybe a float
-        LINE_TYPE:  eg...bgl.GL_LINE_STIPPLE or 
-    '''
-    
-    if LINE_TYPE == "GL_LINE_STIPPLE":  
-        bgl.glLineStipple(4, 0x5555)  #play with this later
-        bgl.glEnable(bgl.GL_LINE_STIPPLE)  
-    
-    
-    current_width = bgl.GL_LINE_WIDTH
-    bgl.glColor4f(*color)
-    bgl.glLineWidth(thickness)
-    bgl.glBegin(bgl.GL_LINE_STRIP)
-    
-    for coord in points:  
-        bgl.glVertex2f(*coord)  
-    
-    bgl.glEnd()  
-    bgl.glLineWidth(1)  
-    if LINE_TYPE == "GL_LINE_STIPPLE":  
-        bgl.glDisable(bgl.GL_LINE_STIPPLE)  
-        bgl.glEnable(bgl.GL_BLEND)  # back to uninterupted lines  
-      
-    return
-
-# Depreciated
-def draw_polyline_from_3dpoints(context, points_3d, color, thickness, LINE_TYPE):
-    '''
-    a simple way to draw a line
-    slow...becuase it must convert to screen every time
-    but allows you to pan and zoom around
-    
-    args:
-        points_3d: a list of tuples representing x,y SCREEN coordinate eg [(10,30),(11,31),...]
-        color: tuple (r,g,b,a)
-        thickness: integer? maybe a float
-        LINE_TYPE:  eg...bgl.GL_LINE_STIPPLE or 
-    '''
-    points = [location_3d_to_region_2d(context.region, context.space_data.region_3d, loc) for loc in points_3d]
-    if LINE_TYPE == "GL_LINE_STIPPLE":  
-        bgl.glLineStipple(4, 0x5555)  #play with this later
-        bgl.glEnable(bgl.GL_LINE_STIPPLE)  
-    
-    bgl.glColor4f(*color)
-    bgl.glLineWidth(thickness)
-    bgl.glBegin(bgl.GL_LINE_STRIP)
-    for coord in points:  
-        bgl.glVertex2f(*coord)  
-    
-    bgl.glEnd()  
-      
-    if LINE_TYPE == "GL_LINE_STIPPLE":  
-        bgl.glDisable(bgl.GL_LINE_STIPPLE)  
-        bgl.glEnable(bgl.GL_BLEND)  # back to uninterupted lines  
-        bgl.glLineWidth(1)
-    return
-    
-def draw_quads_from_3dpoints(context, points_3d, color):
-    '''
-    a simple way to draw a set of quads
-    slow...becuase it must convert to screen every time
-    but allows you to pan and zoom around
-    
-    args:
-        points_3d: a list of tuples as x,y,z
-        color: tuple (r,g,b,a)
-    '''
-    points = [location_3d_to_region_2d(context.region, context.space_data.region_3d, loc) for loc in points_3d]
-    bgl.glEnable(bgl.GL_BLEND)
-    bgl.glColor4f(*color)
-    bgl.glBegin(bgl.GL_QUADS)
-    for coord in points:  
-        bgl.glVertex2f(*coord)
-    bgl.glEnd()  
-    return
 
 def get_path_length(verts):
     '''
