09d516217ca4553bf3e8498af201284d8e0f4fc8
Jonathan Williamson <jonathan@cgcookie.com>
2015-01-05 19:35:11 -0600

    Move help text files to help dir.

--- __init__.py
+++ __init__.py
@@ -2048,7 +2048,7 @@ class CGCOOKIE_OT_contours(bpy.types.Operator):
         self.keymap = key_maps.rtflow_default_keymap_generate()
         
         my_dir = os.path.split(os.path.abspath(__file__))[0]
-        filename = os.path.join(my_dir, "cs_help_txt.txt")
+        filename = os.path.join(my_dir, "help/cs_help_txt.txt")
         if os.path.isfile(filename):
             help_txt = open(filename, mode='r').read()
         else:
@@ -2267,7 +2267,7 @@ class PolystripsUI:
         self.footer_last = ''
         
         my_dir = os.path.split(os.path.abspath(__file__))[0]
-        filename = os.path.join(my_dir, "ps_help_txt.txt")
+        filename = os.path.join(my_dir, "help/ps_help_txt.txt")
         if os.path.isfile(filename):
             help_txt = open(filename, mode='r').read()
         else:
