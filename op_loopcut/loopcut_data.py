e64f377b2042dfc629dcc6154acb2fbb429c6c0d
Patrick Moore <patrick.moore.bu@gmail.com>
2015-07-12 18:36:34 -0400

    initial LoopCut class

---
+++ op_loopcut/loopcut_data.py
@@ -0,0 +1,117 @@
+'''
+Created on Jul 12, 2015
+
+@author: Patrick
+'''
+import bpy
+import bmesh
+
+class LoopCut(object):
+    
+    def __init__(self, context, targ_obj, source_obj = None):
+        self.target_name =targ_obj.name
+        if source_obj:
+            self.source_name = source_obj.name
+            
+        self.face_loop_eds = []
+        self.face_loop_fs = []
+        
+    def find_face_loop(self,bme, ed, select = False):
+        '''takes a bmface and bmedgse'''
+        #reality check
+        if not len(ed.link_faces): return
+        
+        def next_edge(cur_face, cur_ed):
+            ledges = [ed for ed in cur_face.edges]
+            n = ledges.index(cur_ed)
+            j = (n+2) % 4
+            return cur_face.edges[j]
+        
+        def next_face(cur_face, edge):
+            if len(edge.link_faces) == 1: return None
+            next_face = [f for f in edge.link_faces if f != cur_face][0]
+            return next_face
+        
+        loop_eds = []
+        loop_faces = []
+        self.cyclic = False
+        
+        for f in ed.link_faces:
+            if len(f.edges) != 4: continue            
+            eds = [ed.index]
+            fs = [f.index]    
+            
+            f_next = True
+            f_cur = f
+            ed_cur = ed
+            while f_next != f:
+                if select:
+                    f_cur.select_set(True) 
+                    ed_cur.select_set(True)
+                
+                ed_next = next_edge(f_cur, ed_cur)
+                eds += [ed_next.index]
+                f_next = next_face(f_cur, ed_next)
+                if not f_next: break
+                
+                fs += [f_next.index]
+                if len(f_next.verts) != 4:
+                    break
+                
+                ed_cur = ed_next
+                f_cur = f_next
+                
+            #if we looped
+            if f_next == f:
+                self.cyclic = True
+                self.face_loop_fs = fs
+                self.face_loop_eds = eds[:len(eds)-1]
+                return
+            else:
+                loop_faces.append(fs)
+                loop_eds.append(eds)
+        
+        if len(loop_faces) == 2:        
+            self.face_loop_fs = loop_faces[0] +  loop_faces[1].reverse()
+            self.face_loop_eds = loop_eds[0] + loop_eds[1][1:].reverse()
+        else:
+            self.face_loop_fs = loop_faces[0]
+            self.face_loop_eds = loop_eds[0]
+            
+        return
+    
+    def find_edge_loop(self,bme,edge,vert):
+        ''' '''
+        pass
+    
+    def cut_loop(self, bme, select = True):
+
+        eds = [bme.edges[i] for i in self.face_loop_eds]
+        
+        #dummy ed percentage map
+        ed_pcts = {}
+        for ed in eds:
+            ed_pcts[ed] = .5
+        print('bisecting edges')
+        geom =  bmesh.ops.bisect_edges(self.bme, edges = eds,cuts = 1,edge_percents = ed_pcts)
+        
+        new_verts = [ele for ele in geom['geom_split'] if isinstance(ele, bmesh.types.BMVert)]
+        
+        print('connect vert pairs')
+        new_edges = []
+        for i in range(0,len(new_verts)):
+            self.bme.verts.ensure_lookup_table()
+            v_pair = [self.bme.verts[new_verts[i]], self.bme.verts[new_verts[i-1]]]
+            ed_geom = bmesh.ops.connect_vert_pair(self.bme, verts = v_pair, verts_exclude = [], faces_exclude = [])
+            new_edges += ed_geom['edges']
+        
+        if select:
+            for ed in new_edges:
+                ed.select_set(True)
+                
+        return
+    
+    def push_to_edit_mesh(self,bme):
+        target_ob = bpy.data.objects[self.target_name]
+        bme.update_edit_mesh(target_ob.data)
+    
---
+++ op_loopcut/loopcut_ui_draw.py
@@ -0,0 +1,5 @@
+'''
+Created on Jul 12, 2015
+
+@author: Patrick
+'''
