3612edab42e8e8825efa13c60e798f147f378a8c
Patrick Moore <patrick.moore.bu@gmail.com>
2015-07-18 11:42:10 -0400

    Entered all constraints for different patterns
    Permute the edge subdivisions of a patch. Future:-> allowing rotation of a given pattern
    Test all patterns on all permutations. Future:-> allowing pattern switching of a patch

--- pat_patch.py
+++ pat_patch.py
@@ -3,7 +3,33 @@ Created on Jul 15, 2015
 
 @author: Patrick
 '''
+from pulp import LpVariable, LpProblem, LpMinimize, LpMaximize, LpInteger, LpStatus
 
+def permute_subdivs(L):
+    '''
+    returns a list of permutations that preserves the original
+    list order of the sides going CW and CCW around the loop
+    
+    return:
+       permutations - lsit of lists
+       shift_rev - list of tupple (k, rev) k represents the index
+       in the orignial list which is now the 0th element in the
+       permutation.
+    '''
+    perms = []
+    shift_dir = []
+    N = len(L)
+    for i in range(0, N -1):
+        p = L[i:] + L[:i]
+        perms += [p]
+        shift_dir += [(i, 1)]
+        pr = p.copy()
+        pr.reverse()
+        perms += [pr]
+        shift_dir += [((i+N-1) % N, -1)]
+    
+    return perms, shift_dir
+    
 def reducible(edge_subdivs):
     '''
     see section 2.1 in the paper #TODO paper reference    
@@ -69,10 +95,40 @@ class Patch():
         
         return self.valid
         
+    def permute_and_find_solutions(self):
         
+        pat_dict = {}
+        pat_dict[3] = 2
+        pat_dict[4] = 5
+        pat_dict[5] = 4
+        pat_dict[6] = 4
+        
+        perms, rot_dirs = permute_subdivs(self.edge_subdivision)
+        
+        valid_perms = []
+        valid_rot_dirs = []
+        valid_patterns = []
+        valid_solutions = []
+        N = len(self.edge_subdivision)
+        
+        for i, perm in enumerate(perms):
+            for pat in range(0,pat_dict[N]):
+                if N == 6:
+                    sol = PatchSolver6(perm, pat)
+                elif N == 5:
+                    sol = PatchSolver5(perm, pat)
+                elif N == 4:    
+                    sol = PatchSolver4(perm, pat)
+                elif N == 3:    
+                    sol = PatchSolver3(perm, pat)
+                    
+                sol.solve()
+              
     def reduce_input_cornered(self):
         '''
         slices off the biggest quad patches it can at a time
+        this is not needed for patch solving, just proves
+        generality of the approach
         '''
         
         edge_subdivs = self.edge_subdivision.copy()
@@ -102,7 +158,9 @@ class Patch():
     def reduce_input_centered(self):
         '''
         slices off the smallest quad patches it can at a time
-        produces a more centered pole arrangment?
+
+        this is not needed for patch solving, just proves
+        generality of the approach
         '''
         
         edge_subdivs = self.edge_subdivision.copy()
@@ -125,9 +183,38 @@ class Patch():
         print('centered reduced in %i iters' % iters)
    
         self.edges_reduced = new_subdivs
+    
+    def reduce_input_padding(self):
+        '''
+        slices 1 strip off the biggest quad patches it can at a time
+        this is not needed for patch solving, just proves
+        generality of the approach
+        '''
         
-    def identify_patch_pattern(self):
+        edge_subdivs = self.edge_subdivision.copy()
+        print('\n')
+        print('Reduction series for edges')
+        print(edge_subdivs)
+        ks, ds, pots = reducible(edge_subdivs)
         
+        if not len(ks):
+            print('already maximally reduced')
+            self.edges_reduced = edge_subdivs
+            return
+        
+        iters = 0
+        while len(ks) and iters < 10:
+            iters += 1
+            best = pots.index(max(pots))
+            new_subdivs = reduce_edges(edge_subdivs, ks[best], 1)
+            
+            ks, ds, pots = reducible(new_subdivs)
+            print(new_subdivs)
+            edge_subdivs = new_subdivs
+         
+        self.edges_reduced = new_subdivs
+            
+    def identify_patch_pattern(self):   
         n_sides = len(self.edges_reduced)
         unique = set(self.edges_reduced)
         alpha = max(unique)
@@ -141,17 +228,12 @@ class Patch():
                 self.pattern = 1
                 x = (alpha - 4)/2
                 
-            
         elif len(self.edge_subdivision) == 4:
             if alpha == 1:
                 self.pattern = 0
                 
             elif len(unique) == 2 and self.edges_reduced.count(alpha) == 1:
-                #there is only one alpha
-                print('need to ask the authors about this one!')
-                print('but I think you have a choice between pattern 2 and pattern 3')
-                print('there are also editable parameters x and  y that may be used to separate the poles')
-                
+                #there is only one alp
                 x = (alpha - 3) / 2
                 if x == 0:
                     self.pattern = 2
@@ -170,13 +252,12 @@ class Patch():
                 print('[A,B,1,1]  A = B + 2 + 2x')
                 beta = (unique - set([1,alpha])).pop()
                 
-                
-                
+                       
         elif len(self.edge_subdivision) == 5:
             if len(unique) == 2 and alpha ==2:
                 self.pattern = 0
                 print('[A,1,1,1,1] and A = 2')
-                
+                    
             elif len(unique) == 2 and alpha > 2:
                 self.pattern = 2
                 print('[A,1,1,1,1] and A = 4 + 2x')
@@ -186,14 +267,11 @@ class Patch():
                 if beta == alpha -1:
                     self.pattern = 1
                     print('[A,B,1,1,1] and A = B + 1')
-                
                 else:
                     self.pattern = 3
                     print('[A,B,1,1,1] and A = B + 3 + 2x')
                     
-                
-                
-                
+                                   
         elif len(self.edge_subdivision) == 6:
             
             if len(unique) == 1:
@@ -235,20 +313,293 @@ class Patch():
         print('Alpha = %i' % alpha)
         print('Beta = %s' % str(beta))
         print('%i sided patch with pattern #%i' % (n_sides, self.pattern))
-    def identify_tri_pattern(self):
-        pass
-        
-    def identify_quad_pattern(self):
-        pass
-    def identify_pent_pattern(self):
-        pass
-    def identify_hex_pattern(self):
-        if len(set(self.edges_reduced)) == 1:
-            #pattern [1,1,1,1,1,1]
-            self.pattern = 0
-            print('Hex pattern 0')
-        elif len(set(self.edges_reduced)) == 2:
-            pass
-            
-            
+        k0 = self.edges_reduced.index(alpha)
+        print('l_0  side is side #%i, value %i' % (k0, self.edges[k0]))
+
+
+def add_constraints_3p0(prob, L, p0, p1, p2):  #DONE
+    prob +=  p2 + p1            == L[0] - 2, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p0            == L[2] - 1, "Side 2"
     
+def add_constraints_3p1(prob, L, p0, p1, p2, x):     #DONE
+    prob +=  p2 + p1 +2*x       == L[0] - 4, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p0            == L[2] - 1, "Side 2"
+
+def add_constraints_4p0(prob, L, p0, p1, p2, p3):     #DONE
+    prob +=  p3 + p1            == L[0] - 1, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p0            == L[3] - 1, "Side 3"
+
+def add_constraints_4p1(prob, L, p0, p1, p2, p3, x):   #DONE
+    prob +=  p3 + p1 + x        == L[0] - 2, "Side 0"
+    prob +=  p0 + p2 + x        == L[1] - 2, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p0            == L[3] - 1, "Side 3"
+    
+def add_constraints_4p2(prob, L, p0, p1, p2, p3, x, y):   #DONE
+    prob +=  p3 + p1 + x + y    == L[0] - 3, "Side 0"
+    prob +=  p0 + p2 + x        == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p3 + y        == L[3] - 1, "Side 3"
+
+
+def add_constraints_4p3(prob, L, p0, p1, p2, p3, x):   #DONE
+    prob +=  p3 + p1 + 2*x      == L[0] - 3, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p0            == L[3] - 1, "Side 3"
+
+    
+def add_constraints_4p4(prob, L, p0, p1, p2, p3, x, y):   #DONE
+    prob +=  p1 + p1 + 2*x + y  == L[0] - 4, "Side 0"
+    prob +=  p0 + p2 + y        == L[1] - 2, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p0            == L[3] - 1, "Side 3"
+
+def add_constraints_5p0(prob, L, p0, p1, p2, p3, p4):   #DONE
+    prob +=  p4 + p1            == L[0] - 2, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4            == L[3] - 1, "Side 3"
+    prob +=  p3 + p0            == L[4] - 1, "Side 4"
+
+    
+def add_constraints_5p1(prob, L, p0, p1, p2, p3, p4, x): #DONE
+    prob +=  p4 + p1 + x        == L[0] - 2, "Side 0"
+    prob +=  p0 + p2 + x        == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4            == L[3] - 1, "Side 3"
+    prob +=  p3 + p0            == L[4] - 1, "Side 4"
+
+    
+def add_constraints_5p2(prob, L, p0, p1, p2, p3, p4, x): #DONE
+    prob +=  p4 + p1 + 2*x      == L[0] - 4, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4            == L[3] - 1, "Side 3"
+    prob +=  p3 + p0            == L[4] - 1, "Side 4"
+
+    
+def add_constraints_5p3(prob, L, p0, p1, p2, p3, p4, x, y): #DONE
+    prob +=  p4 + p1 + 2*x + y  == L[0] - 5, "Side 0"
+    prob +=  p0 + p2 + y        == L[1] - 2, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4            == L[3] - 1, "Side 3"
+    prob +=  p3 + p0            == L[4] - 1, "Side 4"
+
+                
+def add_constraints_6p0(prob, L, p0, p1, p2, p3, p4, p5, x): #Done
+    prob +=  p5 + p1 + x        == L[0] - 1, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4 + x        == L[3] - 1, "Side 3"
+    prob +=  p3 + p5            == L[4] - 1, "Side 4"
+    prob +=  p4 + p0            == L[5] - 1, "Side 5"
+    
+def add_constraints_6p1(prob, L, p0, p1, p2, p3, p4, p5, x): #DONE
+    prob +=  p5 + p1 + x       == L[0] - 2, "Side 0"
+    prob +=  p0 + p2 + x        == L[1] - 2, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4            == L[3] - 1, "Side 3"
+    prob +=  p3 + p5            == L[4] - 1, "Side 4"
+    prob +=  p4 + p0            == L[5] - 1, "Side 5"
+    
+def add_constraints_6p2(prob, L, p0, p1, p2, p3, p4, p5, x, y):  #DONE
+    prob +=  p5 + p1 + 2*x + y  == L[0] - 3, "Side 0"
+    prob +=  p0 + p2            == L[1] - 1, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4 + y        == L[3] - 1, "Side 3"
+    prob +=  p3 + p5            == L[4] - 1, "Side 4"
+    prob +=  p4 + p0            == L[5] - 1, "Side 5"
+    
+def add_constraints_6p3(prob, L, p0, p1, p2, p3, p4, p5, x, y): #DONE
+    prob +=  p5 + p1 + 2*x + y  == L[0] - 4, "Side 0"
+    prob +=  p0 + p2 + y        == L[1] - 2, "Side 1"
+    prob +=  p1 + p3            == L[2] - 1, "Side 2"
+    prob +=  p2 + p4            == L[3] - 1, "Side 3"
+    prob +=  p3 + p5            == L[4] - 1, "Side 4"
+    prob +=  p4 + p0            == L[5] - 1, "Side 5"
+    
+    
+                
+class PatchSolver6(object):
+    def __init__(self, L, pattern):
+        self.pattern = pattern
+        self.L = L
+        self.prob = LpProblem("N6 Patch", LpMaximize)
+
+        max_p0 = float(min(L[1], L[5]) - 1)
+        max_p1 = float(min(L[0], L[2]) - 1)
+        max_p2 = float(min(L[1], L[3]) - 1)
+        max_p3 = float(min(L[2], L[4]) - 1)
+        max_p4 = float(min(L[3], L[5]) - 1)
+        max_p5 = float(min(L[4], L[0]) - 1)
+
+        p0 = LpVariable("p0",0,max_p0,LpInteger)
+        p1 = LpVariable("p1",0,max_p1,LpInteger)
+        p2 = LpVariable("p2",0,max_p2,LpInteger)
+        p3 = LpVariable("p3",0,max_p3,LpInteger)
+        p4 = LpVariable("p4",0,max_p4,LpInteger)
+        p5 = LpVariable("p5",0,max_p5,LpInteger)
+        
+        x = LpVariable("x",0,None,LpInteger)
+        
+        if pattern in {2,3}:
+            y = LpVariable("y",0,None,LpInteger)
+
+        #first objective, maximize padding  
+        self.prob += p0 + p1 + p2 + p3 + p4 + p5
+        
+        if self.pattern == 0:
+            add_constraints_6p0(self.prob, L, p0, p1, p2, p3, p4, p5, x)
+        elif self.pattern == 1:
+            add_constraints_6p1(self.prob, L, p0, p1, p2, p3, p4, p5, x)
+        elif self.pattern == 2:
+            add_constraints_6p2(self.prob, L, p0, p1, p2, p3, p4, p5, x, y)
+        elif self.pattern == 3:
+            add_constraints_6p3(self.prob, L, p0, p1, p2, p3, p4, p5, x, y)
+    
+    def solve(self):
+        self.prob.solve()
+        
+        if self.prob.status == 1:
+            print(self.L)
+            print('%i sided Patch with Pattern: %i' % (len(self.L),self.pattern))
+            print('Status: ' + LpStatus[self.prob.status])
+            for v in self.prob.variables():
+                print(v.name + ' = ' + str(v.varValue))
+        
+
+class PatchSolver5():
+    def __init__(self, L, pattern):
+        '''
+        L needs to be a list of edge subdivisions with Alpha being L[0]
+        you may need to rotate or reverse L to adequately represent the patch
+        '''
+        self.prob = LpProblem("N5 Patch", LpMaximize)
+        self.pattern = pattern
+        self.L = L
+        
+        max_p0 = float(min(L[4] ,L[1]) - 1)
+        max_p1 = float(min(L[0], L[2]) - 1)
+        max_p2 = float(min(L[1], L[3]) - 1)
+        max_p3 = float(min(L[2], L[4]) - 1)
+        max_p4 = float(min(L[3], L[0]) - 1)
+
+        p0 = LpVariable("p0",0,max_p0,LpInteger)
+        p1 = LpVariable("p1",0,max_p1,LpInteger)
+        p2 = LpVariable("p2",0,max_p2,LpInteger)
+        p3 = LpVariable("p3",0,max_p3,LpInteger)
+        p4 = LpVariable("p4",0,max_p4,LpInteger)
+        x = LpVariable("x",0,None,LpInteger)
+        y = LpVariable("y",0,None,LpInteger)
+
+        #first objective, maximize padding  
+        self.prob += p0 + p1 + p2 + p3 + p4
+        
+        if self.pattern == 0:
+            add_constraints_5p0(self.prob, L, p0, p1, p2, p3, p4)
+        elif self.pattern == 1:
+            add_constraints_5p1(self.prob, L, p0, p1, p2, p3, p4, x)
+        elif self.pattern == 2:
+            add_constraints_5p2(self.prob, L, p0, p1, p2, p3, p4, x)
+        elif self.pattern == 3:
+            add_constraints_5p3(self.prob, L, p0, p1, p2, p3, p4, x, y)
+            
+    def solve(self):
+        self.prob.solve()
+        if self.prob.status == 1:
+            print(self.L)
+            print('%i sided Patch with Pattern: %i' % (len(self.L),self.pattern))
+            print('Status: ' + LpStatus[self.prob.status])
+            for v in self.prob.variables():
+                print(v.name + ' = ' + str(v.varValue))
+class PatchSolver4():
+    def __init__(self, L, pattern):
+        '''
+        L needs to be a list of edge subdivisions with Alpha being L[0]
+        you may need to rotate or reverse L to adequately represent the patch
+        '''
+        self.prob = LpProblem("N4 Patch", LpMaximize)
+        self.pattern = pattern
+        self.L = L
+        
+        max_p0 = float(min(L[3], L[1]) - 1)
+        max_p1 = float(min(L[0], L[2]) - 1)
+        max_p2 = float(min(L[1], L[3]) - 1)
+        max_p3 = float(min(L[2], L[0]) - 1)
+        p0 = LpVariable("p0",0,max_p0,LpInteger)
+        p1 = LpVariable("p1",0,max_p1,LpInteger)
+        p2 = LpVariable("p2",0,max_p2,LpInteger)
+        p3 = LpVariable("p3",0,max_p3,LpInteger)
+
+        if self.pattern != 0:
+            x = LpVariable("x",0,None,LpInteger)
+        
+        if self.pattern in {2,4}:
+            y = LpVariable("y",0,None,LpInteger)
+
+        #first objective, maximize padding 
+        self.prob += p0 + p1 + p2 + p3
+        
+        if self.pattern == 0:
+            add_constraints_4p0(self.prob, L, p0, p1, p2, p3)
+        elif self.pattern == 1:
+            add_constraints_4p1(self.prob, L, p0, p1, p2, p3, x)
+        elif self.pattern == 2:
+            add_constraints_4p2(self.prob, L, p0, p1, p2, p3, x, y)
+        elif self.pattern == 3:
+            add_constraints_4p3(self.prob, L, p0, p1, p2, p3, x)
+        elif self.pattern == 4:
+            add_constraints_4p4(self.prob, L, p0, p1, p2, p3, x, y)
+
+    def solve(self):
+        self.prob.solve()
+        if self.prob.status == 1:
+            print(self.L)
+            print('%i sided Patch with Pattern: %i' % (len(self.L),self.pattern))
+            print('Status: ' + LpStatus[self.prob.status])
+            for v in self.prob.variables():
+                print(v.name + ' = ' + str(v.varValue))
+class PatchSolver3():
+    def __init__(self, L, pattern):
+        '''
+        L needs to be a list of edge subdivisions with Alpha being L[0]
+        you may need to rotate or reverse L to adequately represent the patch
+        '''
+        
+        self.prob = LpProblem("N6 Patch", LpMaximize)
+        self.pattern = pattern
+        self.L = L
+        
+        max_p0 = float(min(L[2], L[1]) - 1)
+        max_p1 = float(min(L[0], L[2]) - 1)
+        max_p2 = float(min(L[1], L[0]) - 1)
+
+        p0 = LpVariable("p0",0,max_p0,LpInteger)
+        p1 = LpVariable("p1",0,max_p1,LpInteger)
+        p2 = LpVariable("p2",0,max_p2,LpInteger)
+       
+        if self.pattern == 1:
+            x = LpVariable("x",0,None,LpInteger)
+
+        #first objective, maximize padding
+        self.prob += p0 + p1 + p2    
+    
+        if self.pattern == 0:
+            add_constraints_3p0(self.prob, L, p0, p1, p2)
+        elif self.pattern == 1:
+            add_constraints_3p1(self.prob, L, p0, p1, p2, x)
+    
+    def solve(self):
+        self.prob.solve()
+        if self.prob.status == 1:
+            print(self.L)
+            print('%i sided Patch with Pattern: %i' % (len(self.L),self.pattern))
+            print('Status: ' + LpStatus[self.prob.status])
+            for v in self.prob.variables():
+                print(v.name + ' = ' + str(v.varValue))
+        
