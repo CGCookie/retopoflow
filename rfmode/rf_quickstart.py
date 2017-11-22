9ebb6228c6d36b40d03ea1f7255bca8bddfb8f55
Jon Denning <gfxcoder@gmail.com>
2017-11-22 13:09:15 -0500

    minor processing of quick start text

--- rfmode/rf_quickstart.py
+++ rfmode/rf_quickstart.py
@@ -1,3 +1,4 @@
+import re
 import bpy
 from ..lib.common_utilities import showErrorMessage
 from ..options import options, help_quickstart
@@ -21,9 +22,27 @@ class OpenQuickStart(bpy.types.Operator):
             # create a log file for error writing
             bpy.data.texts.new(options['quickstart_filename'])
         
+        # simple processing of help_quickstart
+        t = help_quickstart
+        t = re.sub(r'^\n*', r'', t)         # remove leading newlines
+        t = re.sub(r'\n*$', r'', t)         # remove trailing newlines
+        t = re.sub(r'\n\n+', r'\n\n', t)    # make uniform paragraph separations
+        ps = t.split('\n\n')
+        l = []
+        for p in ps:
+            if p.startswith('- '):
+                l += [p]
+                continue
+            lines = p.split('\n')
+            if len(lines) == 2 and (lines[1].startswith('---') or lines[1].startswith('===')):
+                l += [p]
+                continue
+            l += ['  '.join(lines)]
+        t = '\n\n'.join(l)
+        
         # restore data, just in case
         txt = bpy.data.texts[options['quickstart_filename']]
-        txt.from_string(help_quickstart)
+        txt.from_string(t)
         txt.current_line_index = 0
 
         # duplicate the current area then change it to a text edito
