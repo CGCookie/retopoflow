74e2f52c276244d42ca20ffec838b868ae0b2717
Jon Denning <gfxcoder@gmail.com>
2017-11-23 20:10:23 -0500

    how did i miss these!?!

---
+++ rfmode/rf_webissues.py
@@ -0,0 +1,35 @@
+'''
+Copyright (C) 2017 CG Cookie
+http://cgcookie.com
+hello@cgcookie.com
+
+Created by Jonathan Denning, Jonathan Williamson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import bpy
+from ..options import retopoflow_issues_url
+
+class OpenWebIssues(bpy.types.Operator):
+    bl_idname = "wm.open_webissues"
+    bl_label = "Issues Page"
+    
+    @classmethod
+    def poll(cls, context): return True
+
+    def execute(self, context):
+        bpy.ops.wm.url_open(url=retopoflow_issues_url)
+        return {'FINISHED'}
+
