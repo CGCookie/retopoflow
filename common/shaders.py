4fa227ea1ec746824680662552e9f98d227f7c3b
jon denning <jdenning@cse.taylor.edu>
2018-02-21 09:34:39 -0500

    added license and copyright info to top of a few files. added few useful scripts

--- common/shaders.py
+++ common/shaders.py
@@ -1,3 +1,23 @@
+'''
+Copyright (C) 2018 CG Cookie
+
+Created by Jonathan Denning, Jonathan Williamson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
 import bgl
 from ..lib.common_utilities import invert_matrix, matrix_normal, dprint
 from ..lib.common_shader import Shader
