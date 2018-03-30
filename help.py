e314c25108271ce4d3597ca76db51361f5c8b8fb
Jon Denning <gfxcoder@gmail.com>
2018-03-30 12:50:15 -0400

    Patches: updated help

--- help.py
+++ help.py
@@ -367,18 +367,22 @@ help_patches = '''
 The Patches tool helps fill in holes in your topology.
 Select the strip of boundary edges that you wish to fill.
 
-- SELECT / SHIFT+SELECT: select inner edge loop
-- F: fill selected inner edge loop
+- SELECT / SHIFT+SELECT: select edge
+- CTRL+SELECT / CTRL+SHIFT+SELECT: select edge loop
+- F: fill visualized patch
 
 ## Notes
 
-All boundary edges must be adjacent to exactly one face.
-
-The Patches tool currently only handles a limited number of scenarios (listed below).
+The Patches tool currently only handles a limited number of selected regions.
 More support coming soon!
+
+- 2 connected strips in an L-shape
+- 2 parallel strips: the two strips must contain the same number of edges
+- 3 connected strips in a C-shape: first and last strips must contain the same number of edges
+- 4 strips in a rectangular loop: opposite strips must contain the same number of edges
 
 ![](help_patches_2sides_beforeafter.png)
 
-- 2 edges: L shape, | | shape
-- 4 edges: Rectangle shape
+If no pre-visualized regions show after selection, no geometry will be created after pressing F.
+Adjust the Angle parameter to help Patches determine which connected edges should be in the same strip.
 '''
