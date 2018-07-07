a4ac6e0b436e9caeb484884a12957022af9e0957
Jon Denning <gfxcoder@gmail.com>
2018-07-07 16:31:54 -0400

    cleaned up CookieCutter imports

--- cookiecutter/cookiecutter_ui.py
+++ cookiecutter/cookiecutter_ui.py
@@ -17,28 +17,11 @@ https://github.com/CGCookie/retopoflow
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import sys
 import math
-import os
-import re
-import time
 
 import bpy
 import bgl
-from mathutils import Matrix, Vector
-from ..common.maths import BBox
-from bpy.types import Operator, SpaceView3D, bpy_struct
-from bpy.app.handlers import persistent, load_post
 
-from ..lib import common_utilities
-from ..lib.common_utilities import print_exception, print_exception2, showErrorMessage
-from ..lib.classes.logging.logger import Logger
-from ..lib.common_utilities import dprint
-from ..lib.classes.profiler.profiler import profiler
-
-from ..common.ui import set_cursor
-
-from ..common.decorators import stats_report, stats_wrapper, blender_version_wrapper
 from ..common.ui import UI_WindowManager
 
 
