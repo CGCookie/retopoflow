0baa3a7aa605d67acd67bb513afcf0deceac0f99
jon denning <gfxcoder@gmail.com>
2019-08-05 16:37:07 -0400

    sync with addon_common.  added b281 downloader script

--- tools/download_b281.py
+++ tools/download_b281.py
@@ -8,21 +8,19 @@ import shutil
 import tarfile
 import datetime
 
-assert False, 'do not use this anymore'
-
 now = datetime.datetime.now()
 user = os.path.expanduser('~')
 urlroot='https://builder.blender.org'
 url='%s/download/' % urlroot
 tmp='/tmp/download.html'
-blendertar=os.path.join(user, 'software/blender-2.80-%04d%02d%02d.tar.bz2' % (now.year, now.month, now.day))
-blenderpath=os.path.join(user, 'software/blender-2.80-%04d%02d%02d' % (now.year, now.month, now.day))
-blendersym=os.path.join(user, 'software/blender-2.80')
+blendertar=os.path.join(user, 'software/blender-2.81-%04d%02d%02d.tar.bz2' % (now.year, now.month, now.day))
+blenderpath=os.path.join(user, 'software/blender-2.81-%04d%02d%02d' % (now.year, now.month, now.day))
+blendersym=os.path.join(user, 'software/blender-2.81')
 
-print('finding latest blender 2.80')
+print('finding latest blender 2.81')
 tmp=wget.download(url=url, out=tmp, bar=None)
 html=open(tmp, 'rt').read()
-m = re.search(r'/download/blender-2\.80-[0-9a-f]+-linux-glibc[^-]*?-x86_64\.tar\.bz2', html)
+m = re.search(r'/download/blender-2\.81-[0-9a-f]+-linux-glibc[^-]*?-x86_64\.tar\.bz2', html)
 assert m, 'could not find match'
 
 url = '%s/%s' % (urlroot, m.group(0))
