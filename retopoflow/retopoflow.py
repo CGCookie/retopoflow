c51f03e92d0366ca2470aee22d6073ebed3a3445
jon denning <gfxcoder@gmail.com>
2019-08-06 14:16:45 -0400

    wip: input text!

--- retopoflow/retopoflow.py
+++ retopoflow/retopoflow.py
@@ -102,31 +102,35 @@ class VIEW3D_OT_RetopoFlow(CookieCutter):
             def width_decrease(e):
                 self.ui_main.width = self.ui_main.width_pixels - 50
             self.ui_main.append_child(ui.img(src='contours_32.png'))
-            self.ui_main.append_child(ui.img(src='polystrips_32.png', style='width:26px; height:26px'))
-            self.ui_main.append_child(ui.button(label="Click on me, but do NOT double click!", on_mouseclick=mouseclick, on_mousedblclick=mousedblclick, on_mousedown=mousedown, on_mouseup=mouseup))
-            self.ui_main.append_child(ui.button(label="FOO", style="display:block", children=[ui.button(label="BAR", style="display:block")]))
-            self.ui_main.append_child(ui.button(id="alpha0", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 0"))
-            self.ui_main.append_child(ui.button(id="alpha1", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 1"))
-            self.ui_main.append_child(ui.button(id="alpha2", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 2"))
-            self.ui_main.append_child(ui.button(id="alpha3", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 3"))
+            # self.ui_main.append_child(ui.img(src='polystrips_32.png', style='width:26px; height:26px'))
+            # self.ui_main.append_child(ui.button(label="Click on me, but do NOT double click!", on_mouseclick=mouseclick, on_mousedblclick=mousedblclick, on_mousedown=mousedown, on_mouseup=mouseup))
+            # self.ui_main.append_child(ui.button(label="FOO", style="display:block", children=[ui.button(label="BAR", style="display:block")]))
+            # self.ui_main.append_child(ui.button(id="alpha0", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 0"))
+            # self.ui_main.append_child(ui.button(id="alpha1", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 1"))
+            # self.ui_main.append_child(ui.button(id="alpha2", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 2"))
+            # self.ui_main.append_child(ui.button(id="alpha3", label="ABCDEFGHIJKLMNOPQRSTUVWXYZ 3"))
             self.ui_main.append_child(ui.br())
             self.ui_main.append_child(ui.button(label="Reload Styles Now", on_mouseclick=reload_stylings))
-            self.ui_main.append_child(ui.p(innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
-            self.ui_main.append_child(ui.textarea(innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+            # self.ui_main.append_child(ui.p(innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+            # self.ui_main.append_child(ui.textarea(innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
 
-            self.ui_tools = ui.framed_dialog(label='Tools')
-            self.document.body.append_child(self.ui_tools)
+            # self.ui_tools = ui.framed_dialog(id='toolsframe', label='Tools')
+            # self.document.body.append_child(self.ui_tools)
+            self.ui_tools = self.ui_main
             state_p = self.ui_tools.append_child(ui.p())
             state_p.append_child(ui.span(innerText='State:'))
             self.state = state_p.append_child(ui.span(innerText='???'))
             self.ui_tools.append_child(ui.p(innerText="Foo Bar Baz"))
-            self.ui_tools.append_child(ui.textarea(innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+            ui_input = self.ui_tools.append_child(ui.input_text(id="inputtext", value="test2", innerText="test2"))
+            ui_input.value = 'test'
             div_width = self.ui_tools.append_child(ui.div())
             div_width.append_child(ui.span(innerText='width:'))
             div_width.append_child(ui.button(label='+', on_mouseclick=width_increase))
             div_width.append_child(ui.button(label='-', on_mouseclick=width_decrease))
+            div_width.append_child(ui.button(label='=')).add_pseudoclass('disabled')
             self.ui_tools.right = 0
             self.ui_tools.top = 0
+            print(self.document.body.structure())
         setup_ui()
 
 
@@ -190,10 +194,14 @@ class VIEW3D_OT_RetopoFlow_Tool(WorkSpaceTool):
     bl_icon = "ops.mesh.polybuild_hover"
     bl_widget = None
     bl_keymap = (
-        ("view3d.select_circle", {"type": 'LEFTMOUSE', "value": 'PRESS'},
-         {"properties": [("wait_for_input", False)]}),
-        ("view3d.select_circle", {"type": 'LEFTMOUSE', "value": 'PRESS', "ctrl": True},
-         {"properties": [("mode", 'SUB'), ("wait_for_input", False)]}),
+        ("view3d.select_circle",
+            {"type": 'LEFTMOUSE', "value": 'PRESS'},
+            {"properties": [("wait_for_input", False)]}
+        ),
+        ("view3d.select_circle",
+            {"type": 'LEFTMOUSE', "value": 'PRESS', "ctrl": True},
+            {"properties": [("mode", 'SUB'), ("wait_for_input", False)]}
+        ),
     )
 
     def draw_settings(context, layout, tool):
