bfbdf1f9f62d024ae679e53cc9c1f6fa151ffc47
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-03-03 14:20:10 +0100

    update init pxd and pyx files from cython bl_types submodule

--- retopoflow/cy/bl_types/__init__.pyx
+++ retopoflow/cy/bl_types/__init__.pyx
@@ -1,17 +1,18 @@
 # distutils: language=c++
 # cython: language_level=3
 
-# Import version from bpy at compile time
+# Import directly from the local package
 from bpy.app import version
 cdef int BLENDER_VERSION = version[0] * 100 + version[1] * 10 + version[2]
 
+# Import the structures directly
 from retopoflow.cy.bl_types.vec_types cimport rcti, rctf, BoundBox
 from retopoflow.cy.bl_types.list_base cimport ListBase
 from retopoflow.cy.bl_types.bmesh_types cimport BMVert, BMEdge, BMFace, BMesh, BMLoop, BMHeader
 from retopoflow.cy.bl_types.bmesh_py_wrappers cimport BPy_BMesh
 from retopoflow.cy.bl_types.bmesh_flags cimport BMElemHFlag, BM_elem_flag_test
 
-# VERSIONING
+# VERSIONING - use absolute imports instead of relative
 if BLENDER_VERSION >= 440:
     from .v440 cimport ARegion, RegionView3D, View2D
 elif BLENDER_VERSION >= 430:
