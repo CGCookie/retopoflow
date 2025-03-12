02705ee8a1008327a59fc53e97d0a13d6951a349
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-03-12 17:45:29 +0100

    Cython: expand vector utils

--- retopoflow/cy/vector_utils.pxd
+++ retopoflow/cy/vector_utils.pxd
@@ -10,3 +10,6 @@ cdef struct bVec3:
 
 cdef float vec3_dot(const float* a, const float* b) noexcept nogil
 cdef void vec3_normalize(float* v) noexcept nogil
+
+cdef void copy_v4_to_v3(float[4] vec4, float[3] vec3) noexcept nogil
+cdef void div_v3_f(float[3] vec3, float value) noexcept nogil
--- retopoflow/cy/vector_utils.pyx
+++ retopoflow/cy/vector_utils.pyx
@@ -20,3 +20,14 @@ cdef void vec3_normalize(float* v) noexcept nogil:
         v[0] /= length
         v[1] /= length
         v[2] /= length
+
+
+cdef void copy_v4_to_v3(float[4] vec4, float[3] vec3) noexcept nogil:
+    vec3[0] = vec4[0]
+    vec3[1] = vec4[1]
+    vec3[2] = vec4[2]
+
+cdef void div_v3_f(float[3] vec3, float value) noexcept nogil:
+    vec3[0] = vec3[0] / value
+    vec3[1] = vec3[1] / value
+    vec3[2] = vec3[2] / value
