522768d31c4fc8497f32eee4416cf86f4c2de70e
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-04-23 14:33:58 +0200

    Cython compiler: use the same numpy and cython versions than the target Blender version

--- cy_compile.bat
+++ cy_compile.bat
@@ -7,7 +7,12 @@ set PYTHON_PATH="%LOCALAPPDATA%\Programs\Python\Python311\python.exe"
 :: Get the directory of the batch script
 cd "%~dp0"
 
-"%PYTHON_PATH%" -m pip install --upgrade cython numpy
+:: Blender 4.3 - Python 3.11.9 - Cython '0.29.30' - Numoy '1.24.x'
+:: Blender 4.4 - Python 3.11.11 - Cython '3.0.11' - Numoy '1.26.4'
+set CYTHON_VERSION=3.0.11
+set NUMPY_VERSION=1.26.4
+
+"%PYTHON_PATH%" -m pip install --upgrade numpy==%NUMPY_VERSION% cython==%CYTHON_VERSION%
 "%PYTHON_PATH%" cy_setup.py build_ext -i -v --inplace --force --dev
 
 if %errorlevel% neq 0 (
