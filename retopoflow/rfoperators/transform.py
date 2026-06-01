028f64f8418a62ef353ac6d3052487ce970a5c30
jlampel <jlampel@autotroph.com>
2026-06-01 12:23:24 -0700

    Fix slide redo panel

--- retopoflow/rfoperators/transform.py
+++ retopoflow/rfoperators/transform.py
@@ -64,7 +64,7 @@ class RFOperator_Translate(RFOperator):
     bl_label = 'Translate'
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
-    bl_options = set()
+    bl_options = {'UNDO'}
 
     rf_keymaps = [
         (f'{bl_idname}_grab', {'type': 'G', 'value': 'PRESS'}, None),
@@ -176,25 +176,6 @@ class RFOperator_Translate(RFOperator):
                     #self.bm.select_history.validate()
                     bmops.flush_selection(self.bm, self.em)
 
-        # Handle loop selection
-        active_tool = context.workspace.tools.from_space_view3d_mode('EDIT_MESH', create=False)
-        if active_tool and self.used_keyboard == False:
-            tool_props = active_tool.operator_properties(active_tool.idname)
-            self.select_loops = getattr(tool_props, 'select_loops', False)
-        if self.select_loops:
-            selected_edges = bmops.get_all_selected_bmedges(self.bm)
-            selected_faces = bmops.get_all_selected_bmfaces(self.bm)
-            if len(selected_edges) and not len(selected_faces):
-                prev_mode = tuple(context.tool_settings.mesh_select_mode)
-                context.tool_settings.mesh_select_mode = (False, True, False)
-                if bpy.app.version >= (5, 1, 0):
-                    bpy.ops.mesh.select_edge_loop_multi(delimit_edge_loop={'NGONS', 'INNER_CORNERS', 'OUTER_CORNERS'})
-                else:
-                    bpy.ops.mesh.loop_multi_select(ring=False)
-                context.tool_settings.mesh_select_mode = prev_mode
-                bmops.flush_selection(self.bm, self.em)
-                self.use_slide = True
-
         self.bmvs = list(bmops.get_all_selected_bmverts(self.bm))
         # self.bmvs_co_orig = [Vector(bmv.co) for bmv in self.bmvs]
         # self.bmvs_co2d_orig = [location_3d_to_region_2d(context.region, context.region_data, (self.matrix_world @ Vector((*bmv.co, 1.0))).xyz) for bmv in self.bmvs]
@@ -288,6 +269,24 @@ class RFOperator_Translate(RFOperator):
         # Cursors.set('NONE')  # PAINT_CROSS
 
     def update(self, context, event):
+        # Handle loop selection
+        active_tool = context.workspace.tools.from_space_view3d_mode('EDIT_MESH', create=False)
+        if active_tool and self.used_keyboard == False:
+            tool_props = active_tool.operator_properties(active_tool.idname)
+            self.select_loops = getattr(tool_props, 'select_loops', False)
+        if self.select_loops:
+            selected_edges = bmops.get_all_selected_bmedges(self.bm)
+            selected_faces = bmops.get_all_selected_bmfaces(self.bm)
+            if len(selected_edges) and not len(selected_faces):
+                prev_mode = tuple(context.tool_settings.mesh_select_mode)
+                context.tool_settings.mesh_select_mode = (False, True, False)
+                if bpy.app.version >= (5, 1, 0):
+                    bpy.ops.mesh.select_edge_loop_multi(delimit_edge_loop={'NGONS', 'INNER_CORNERS', 'OUTER_CORNERS'})
+                else:
+                    bpy.ops.mesh.loop_multi_select(ring=False)
+                context.tool_settings.mesh_select_mode = prev_mode
+                bmops.flush_selection(self.bm, self.em)
+                self.use_slide = True
 
         if self.use_slide or (event.type == 'G' and event.value == 'PRESS'):
             selected_edges = bmops.get_all_selected_bmedges(self.bm)
